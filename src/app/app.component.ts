2958b59fa178cb1801e768f1e03b481696c0cab6
J Kanani <kanani.joshua@gmail.com>
2018-11-03 15:51:56 -0500

    overhaul travis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Finall trial of hyracbox resources';
+  title = 'hyracresources';
 }
